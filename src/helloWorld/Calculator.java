1f02f53957dce0a359d48e6e522916209e7836e7
mrbhagwani <mrbhagwani@gmail.com>
2022-10-31 12:21:35 +0000

    new calculator

---
+++ src/helloWorld/Calculator.java
@@ -0,0 +1,34 @@
+package helloWorld;
+
+public class Calculator {
+
+    private int first;
+    private int second;
+
+    public Calculator(int firstNumber, int secondNumber) {
+        first = firstNumber;
+        second = secondNumber;
+    }
+
+    public int add() {
+        return first + second;
+    }
+
+    public int mul() {
+        return first * second;
+    }
+
+    public int sub() {
+        return first - second;
+    }
+
+    public int div() {
+        return first / second;
+    }
+    public int reverseSub() {
+        return second - first;
+    }
+    public int reverseDiv() {
+        return second / first;
+    }
+}
